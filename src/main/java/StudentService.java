84132cfb3dfc6c49f235ac62ae146e166265324b
Chiara Mezzavilla <2512470+samurang87@users.noreply.github.com>
2024-12-06 12:00:46 +0100

    feat: Add exception throwing exercise

--- src/main/java/StudentService.java
+++ src/main/java/StudentService.java
@@ -1,4 +1,15 @@
+/*
+Step 1: Extend the StudentDB with a findById method with a parameter id that returns
+a Student. For the self-confident: If you want to proceed test-driven (TDD, test-driven
+ development), you can also start with step 3.
+Step 2: The findById method should throw an appropriate exception if no student with
+the Id exists. Also consider what the "appropriate exception" might be. A
+Checked Exception or an Unchecked Exception? An instance of a predefined or
+a self-written exception class?
+Step 3: Write at least one test that verifies this behavior.*/
+
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public class StudentService {
@@ -12,5 +23,13 @@ public class StudentService {
 
     public List<Student> getAllStudents(){
         return repo.getAllStudents();
+    }
+
+    public Student findStudentById(String number) throws StudentNotFoundException {
+        Optional<Student> student = repo.findStudentById(number);
+        if (student.isEmpty()) {
+            throw new StudentNotFoundException("Student with id " + number + " not found");
+        }
+        return student.get();
     }
 }
---
+++ src/test/StudentServiceTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StudentServiceTest {
+
+    @Test
+    void findStudentById_returnsStudentWhenIdFound() {
+        Student student = Student.builder()
+                .name("Alice")
+                .subject("Math")
+                .build();
+        StudentService studentService = new StudentService();
+        Student savedStudent = studentService.addNewStudent(student);
+
+        Student result = studentService.findStudentById(savedStudent.id());
+
+        Assertions.assertEquals(savedStudent, result);
+    }
+
+    @Test
+    void findStudentById_throwsExceptionWhenIdNotFound() {
+        StudentService studentService = new StudentService();
+
+        Assertions.assertThrows(StudentNotFoundException.class, () -> {
+            studentService.findStudentById("1");
+        });
+    }
+}
